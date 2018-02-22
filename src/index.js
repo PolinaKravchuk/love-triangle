e938793e5ec44b3d40521d151c6544f886e632b6
Polina <polina8kravchuk@gmail.com>
2018-02-22 10:51:33 +0300

    t

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */ 
- var first, second, third, f_p, s_p, th_p, mass = [], count = 0;
+ var first, second, third, f_p, s_p, th_p, mass = [], count = 0; 
 module.exports = function getLoveTrianglesCount(preferences = []) {
  
 	for (var i = 0; i < preferences.length; i++){
@@ -10,18 +10,18 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   	first = preferences[i];
   	f_p = i + 1;
 	  	
-		  	if (mass.indexOf(f_p) == -1 && first != f_p){
+		  	if (mass.indexOf(f_p) == -1 && first != f_p+1){
 
 			  	second = preferences[first-1];
 			  	s_p = first-1;
 
 			  	
-				  	if (mass.indexOf(s_p) == -1 && second != s_p){
+				  	if (mass.indexOf(s_p) == -1 && second != s_p+1){
 
 					  	third = preferences[second-1];
 					  	th_p = second - 1;
 
-					  	if (mass.indexOf(th_p) == -1 && third != th_p){
+					  	if (mass.indexOf(th_p) == -1 && third != th_p+1){
 
 						  	if (third == f_p) {
 						  		count++;
